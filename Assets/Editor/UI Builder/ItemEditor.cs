275cf39888257205b92225f0570ba5096f199b1c
HerseyHo <hersey_ho@outlook.com>
2025-06-10 16:46:20 +0800

    add listView item

--- Assets/Editor/UI Builder/ItemEditor.cs
+++ Assets/Editor/UI Builder/ItemEditor.cs
@@ -2,10 +2,19 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using System.Collections.Generic;
+using System;
 
 
 public class ItemEditor : EditorWindow
 {
+    private ItemDataList_SO dataBase;
+    private List<ItemDetails> itemList = new List<ItemDetails>();
+    private VisualTreeAsset itemRowTemplate;
+
+    //获得VisualElement
+    private ListView itemListView;
+
     [MenuItem("QuantuanRaven/ItemEditor")]
     public static void ShowExample()
     {
@@ -27,11 +36,54 @@ public class ItemEditor : EditorWindow
         VisualElement labelFromUXML = visualTree.Instantiate();
         root.Add(labelFromUXML);
 
-        //// A stylesheet can be added to a VisualElement.
-        //// The style will be applied to the VisualElement and all of its children.
-        //var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/UI Builder/ItemEditor.uss");
-        //VisualElement labelWithStyle = new Label("Hello World! With Style");
-        //labelWithStyle.styleSheets.Add(styleSheet);
-        //root.Add(labelWithStyle);
+        //拿到模板数据
+        itemRowTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Builder/ItemRowTemplate.uxml");
+
+        //变量赋值
+        itemListView = root.Q<VisualElement>("ItemList").Q<ListView>("ListView");
+
+        //加载数据
+        LoadDataBase();
+
+        //生成ListView
+        GenerateListView();
+    }
+    //在创建面板之前拿到数据
+    private void LoadDataBase()
+    {
+        var dataArray = AssetDatabase.FindAssets("ItemDataList_SO"); 
+
+        if(dataArray.Length > 1)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(dataArray[0]);
+            dataBase = AssetDatabase.LoadAssetAtPath(path, typeof(ItemDataList_SO)) as ItemDataList_SO;
+        }
+
+        itemList = dataBase.itemDetailsList;
+
+        //如果不标记则无法保存数据
+        EditorUtility.SetDirty(dataBase);
+        //Debug.Log(itemList[0].itemID);
+    }
+
+    private void GenerateListView()
+    {
+        Func<VisualElement> makeItem = () => itemRowTemplate.CloneTree();
+
+        Action<VisualElement, int> bindItem = (e, i) =>
+        {
+            if (i < itemList.Count)
+            {
+                if (itemList[i].itemIcon != null)
+                    e.Q<VisualElement>("Icon").style.backgroundImage = itemList[i].itemIcon.texture;
+                e.Q<Label>("Name").text = itemList[i] == null ? "NO ITEM" : itemList[i].itemName;
+                e.Q<Label>("ID").text = itemList[i] == null ? "NO ITEM" : itemList[i].itemID;
+            }
+        };
+
+        itemListView.fixedItemHeight = 80;
+        itemListView.itemsSource = itemList;
+        itemListView.makeItem = makeItem;
+        itemListView.bindItem = bindItem;
     }
 }
