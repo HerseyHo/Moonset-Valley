5c70069c9928d910477defa446467d462de736e4
HerseyHo <hersey_ho@outlook.com>
2025-06-17 17:29:11 +0800

    add Inventory system

---
+++ Assets/QuantumRaven/Scripts/UI/SlotUI.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SlotUI : MonoBehaviour
+{
+    [Header("组件获取")]
+
+    [SerializeField] private Image slotImage;
+
+    [SerializeField] private TextMeshProUGUI amountText;
+
+    [SerializeField] private Image slotHightlight;
+
+    [SerializeField] private Button button;
+
+    [Header("格子类型")]
+    public SlotType slotType;
+
+    public bool isSelected;
+
+    //物品信息
+    public ItemDetails itemDetails;
+
+    public int itemAmount;
+
+    private void Start()
+    {
+        isSelected = false;
+        if(itemDetails.itemID == "")
+        {
+            UpdateEmptySlot();
+        }
+    }
+
+    /// <summary>
+    /// 更新格子UI和信息
+    /// </summary>
+    /// <param name="item">物品信息</param>
+    /// <param name="amount">物品数量</param>
+    public void UpdateSlot(ItemDetails item, int amount)
+    {
+        itemDetails = item;
+        slotImage.sprite = item.itemIcon;
+        itemAmount = amount;
+        amountText.text = amount.ToString();
+        button.interactable = true;
+    }
+
+    /// <summary>
+    /// 将Slot更新为空
+    /// </summary>
+    public void UpdateEmptySlot()
+    {
+        if (isSelected)
+        {
+            isSelected = false;
+        }
+
+        slotImage.enabled = false;
+        amountText.text = string.Empty;
+        button.interactable = false;
+    }
+}
--- Assets/QuantumRaven/Scripts/Utilities/Enums.cs
+++ Assets/QuantumRaven/Scripts/Utilities/Enums.cs
@@ -1,4 +1,11 @@
+//物品类型
 public enum ItemType
 {
     Seed, Commodity, Furniture, HeoTool, ChopTool, BreakTool, ReapTool, WaterTool, CollectTool, ReapableScenery
+}
+
+//背包类型
+public enum SlotType
+{
+    Bag, Box, Shop, ActionBar
 }
