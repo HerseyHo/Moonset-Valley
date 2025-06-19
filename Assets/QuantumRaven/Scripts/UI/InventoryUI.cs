f7b1400790dabe3067d14a182a3af2016a6f1366
HerseyHo <hersey_ho@outlook.com>
2025-06-19 14:18:14 +0800

    add menu hide control

--- Assets/QuantumRaven/Scripts/UI/InventoryUI.cs
+++ Assets/QuantumRaven/Scripts/UI/InventoryUI.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace MoonsetValley.Inventory
 {
     public class InventoryUI : MonoBehaviour
     {
+        [Header("玩家背包UI")]
+        [SerializeField] private GameObject bagUI;
+        private bool bagOpened;
+
+        [Header("控制菜单切换")]
+        [SerializeField] private GameObject[] MenuTabItem;
+
+        public string currentPage;
+
         [SerializeField] private SlotUI[] playerSlots;
 
         private void OnEnable()
@@ -26,13 +37,24 @@ namespace MoonsetValley.Inventory
             {
                 playerSlots[i].slotIndex = i;
             }
+            bagOpened = bagUI.activeInHierarchy;
+
         }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                OpenBagUI();
+            }
+        }
+
         private void OnUpdateInventoryUI(InventoryLocation location, List<InventoryItem> list)
         {
             switch (location)
             {
                 case InventoryLocation.Player:
-                    for (int i = 0;i < playerSlots.Length;i++)
+                    for (int i = 0; i < playerSlots.Length; i++)
                     {
                         if (list[i].itemAmount > 0)
                         {
@@ -44,6 +66,63 @@ namespace MoonsetValley.Inventory
                             playerSlots[i].UpdateEmptySlot();
                         }
                     }
+                    break;
+            }
+        }
+        /// <summary>
+        /// 控制菜单打开关闭
+        /// </summary>
+        public void OpenBagUI()
+        {
+            bagOpened = !bagOpened;
+
+            bagUI.SetActive(bagOpened);
+
+            MenuSelect(currentPage);
+        }
+
+        public void MenuSelect(string key)
+        {
+            currentPage = key;
+            switch (key)
+            {
+                case "Information":
+                    MenuTabItem[0].SetActive(true);
+                    MenuTabItem[1].SetActive(false);
+                    MenuTabItem[2].SetActive(false);
+                    MenuTabItem[3].SetActive(false);
+                    MenuTabItem[4].SetActive(false);
+                    break;
+                case "Mission":
+                    MenuTabItem[0].SetActive(false);
+                    MenuTabItem[1].SetActive(true);
+                    MenuTabItem[2].SetActive(false);
+                    MenuTabItem[3].SetActive(false);
+                    MenuTabItem[4].SetActive(false);
+                    break;
+                case "Inventory":
+                    MenuTabItem[0].SetActive(false);
+                    MenuTabItem[1].SetActive(false);
+                    MenuTabItem[2].SetActive(true);
+                    MenuTabItem[3].SetActive(false);
+                    MenuTabItem[4].SetActive(false);
+                    break;
+                case "BluePrint":
+                    MenuTabItem[0].SetActive(false);
+                    MenuTabItem[1].SetActive(false);
+                    MenuTabItem[2].SetActive(false);
+                    MenuTabItem[3].SetActive(true);
+                    MenuTabItem[4].SetActive(false);
+                    break;
+                case "Settings":
+                    MenuTabItem[0].SetActive(false);
+                    MenuTabItem[1].SetActive(false);
+                    MenuTabItem[2].SetActive(false);
+                    MenuTabItem[3].SetActive(false);
+                    MenuTabItem[4].SetActive(true);
+                    break;
+                default:
+                    Console.WriteLine("...");
                     break;
             }
         }
