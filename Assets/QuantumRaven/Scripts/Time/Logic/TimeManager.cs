067ee3a8787e9fd0af9313aabfbbc461ed64c5f9
HerseyHome <hersey_ho@outlook.com>
2025-06-22 23:53:27 +0800

    add game logic

---
+++ Assets/QuantumRaven/Scripts/Time/Logic/TimeManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private int gameSecond, gameMinute, gameHour, gameDay, gameMonth, gameYear;
+
+    public bool gameClockPause;
+
+    private float tikTime;
+
+    private void Awake()
+    {
+        NewGameTime();
+    }
+
+    private void Update()
+    {
+
+        if (!gameClockPause)
+        {
+            tikTime += Time.deltaTime;
+
+            if (tikTime >= Settings.secondThreshold)
+            {
+                tikTime -= Settings.secondThreshold;
+                UpdateGameTime();
+            }
+        }
+    }
+
+    private void NewGameTime()
+    {
+        gameSecond = 0;
+        gameMinute = 0;
+        gameHour = 7;
+        gameDay = 1;
+        gameMonth = 1;
+        gameYear = 1;
+    }
+
+    private void UpdateGameTime()
+    {
+        gameSecond++;
+        if (gameSecond > Settings.secondHold)
+        {
+            gameMinute++;
+            gameSecond = 0;
+            if (gameMinute > Settings.minuteHold)
+            {
+                gameHour++;
+                gameMinute = 0;
+                if (gameHour > Settings.hourHold)
+                {
+                    gameDay++;
+                    gameHour = 0;
+                    if (gameDay > Settings.dayHold)
+                    {
+                        gameMonth++;
+                        gameDay = 1;
+                        if (gameMonth > 12)
+                        {
+                            gameMonth = 1;
+                            gameYear++;
+                        }
+
+                    }
+                }
+            }
+        }
+        //Debug.Log("Second:" + gameSecond + " Minute:" + gameMinute);
+    }
+}
---
+++ Assets/QuantumRaven/Scripts/Utilities/Settings.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings
+{
+    //时间相关
+    public const float secondThreshold = 0.1f;   //数值越小时间越快
+
+    public const int secondHold = 59;
+
+    public const int minuteHold = 59;
+
+    public const int hourHold = 59;
+
+    public const int dayHold = 30;
+}
